33aab1fb09a6774c32f7b8a73dff555dc66f13f1
Ben Zipperer <benzipperer@gmail.com>
2023-01-10 00:52:00 -0500

    add groups

--- low_wage_data.R
+++ low_wage_data.R
@@ -4,50 +4,61 @@ library(epidatatools)
 library(lubridate)
 library(haven)
 
-org <- load_org(2021:2022) |>
+org_raw <- load_org(2021:2022) |>
   mutate(my_wage = wageotc) |>
   filter(my_wage > 0) |>
   mutate(month_date = ym(paste(year, month)))
+
 max_date <- org |>
   summarize(max(month_date)) |>
   pull()
-
 min_date <- max_date - months(11)
 
+state_follows_fed_min <- c("AL", "GA", "ID", "IN", "IA", "KS", "KY", "LA", "MS", "NH", "NC", "ND", "OK", "PA", "SC", "TN", "TX", "UT", "WI", "WY")
+
+org_clean <- org_raw %>% 
+  filter(month_date >= min_date & month_date <= max_date) |>
+  mutate(all = 1) |>
+  mutate(all = labelled(all, c("All workers" = 1))) |>
+  mutate(union = labelled(union, c("In a union" = 1, "Not in a union" = 0))) |>
+  mutate(part_time = if_else(ftptstat >= 6 & ftptstat <= 10, 1, 0)) |>
+  mutate(part_time = labelled(part_time, c("Works part-time" = 1, "Works full-time" = 0))) |>
+  mutate(new_educ = ifelse(educ <= 4, educ, 4)) |>
+  mutate(new_educ = labelled(new_educ, c(
+    "Less than high school diploma" = 1,
+    "High school diploma" = 2,
+    "Some college" = 3,
+    "College or advanced degree" = 4
+  ))) |>
+  mutate(new_age = case_when(
+    age >= 16 & age <= 24 ~ 1,
+    age >= 25 & age <= 34 ~ 2,
+    age >= 35 & age <= 44 ~ 3,
+    age >= 45 & age <= 54 ~ 4,
+    age >= 55 & age <= 64 ~ 5,
+    age >= 65 ~ 6
+  )) |> 
+  mutate(new_age = labelled(new_age , c(
+    "Ages 16-24" = 1,
+    "Ages 25-34" = 2,
+    "Ages 35-44" = 3,
+    "Ages 45-54" = 4,
+    "Ages 55-64" = 5,
+    "Ages 65 and above" = 6
+  ))) |>
+  mutate(above_fedmw = if_else(
+    as_factor(statefips) %in% state_follows_fed_min, 0, 1
+  )) |>
+  mutate(above_fedmw = labelled(above_fedmw, c(
+    "State minimum exceeds federal minimum" = 1,
+    "State minimum follows federal minimum" = 0
+  )))
+
 create_slice <- function(threshold) {
-  org |>
-    filter(month_date >= min_date & month_date <= max_date) |>
+  org_clean |>
     mutate(low_wage = my_wage < threshold) |>
-    mutate(all = 1) |>
-    mutate(all = labelled(all, c("All workers" = 1))) |>
-    mutate(union = labelled(union, c("In a union" = 1, "Not in a union" = 0))) |>
-    mutate(part_time = if_else(ftptstat >= 6 & ftptstat <= 10, 1, 0)) |>
-    mutate(part_time = labelled(part_time, c("Works part-time" = 1, "Works full-time" = 0))) |>
-    mutate(new_educ = ifelse(educ <= 4, educ, 4)) |>
-    mutate(new_educ = labelled(new_educ, c(
-      "Less than high school diploma" = 1,
-      "High school diploma" = 2,
-      "Some college" = 3,
-      "College or advanced degree" = 4
-    ))) |>
-    mutate(new_age = case_when(
-      age >= 16 & age <= 24 ~ 1,
-      age >= 25 & age <= 34 ~ 2,
-      age >= 35 & age <= 44 ~ 3,
-      age >= 45 & age <= 54 ~ 4,
-      age >= 55 & age <= 64 ~ 5,
-      age >= 65 ~ 6
-    )) |> 
-    mutate(new_age = labelled(new_age , c(
-      "Ages 16-24" = 1,
-      "Ages 25-34" = 2,
-      "Ages 35-44" = 3,
-      "Ages 45-54" = 4,
-      "Ages 55-64" = 5,
-      "Ages 65 and above" = 6
-    ))) |>
     summarize_groups(
-      all|wbhao|female|union|part_time|new_educ|new_age, 
+      all|wbhao|female|union|part_time|new_educ|new_age|above_fedmw|region, 
       low_wage_share = weighted.mean(low_wage, w = orgwgt),
       low_wage_count = round(sum(low_wage * orgwgt / 12) / 1000)*1000
     ) |>
@@ -80,7 +91,9 @@ results <- map_dfr(15:25, create_slice) |>
     category_group == "wbhao" ~ "Race and ethnicity",
     category_group == "part_time" ~ "Part-time status",
     category_group == "new_educ" ~ "Education",
-    category_group == "new_age" ~ "Age group"
+    category_group == "new_age" ~ "Age group",
+    category_group == "above_fedmw" ~ "State minimum wage",
+    category_group == "region" ~ "Region"
   )) |>
   arrange(low_wage_threshold, desc(priority), category_group, category) 
 
